bb163b80617899051ae2b230b0d94cf0cb29fc5d
maximan81 <ericmaximan@gmail.com>
2021-07-02 18:00:55 +0300

    update apiHelpers

--- src/utils/agent.js
+++ src/utils/agent.js
@@ -2,8 +2,8 @@ import superagentPromise from "superagent-promise";
 import _superagent from "superagent";
 
 const superagent = superagentPromise(_superagent, global.Promise);
-const API_ROOT = "http://localhost:8000/api";
-//const API_ROOT = "https://friendly-beaver-14391.herokuapp.com/api";
+//const API_ROOT = "http://localhost:8000/api";
+const API_ROOT = "https://friendly-beaver-14391.herokuapp.com/api";
 const responseBody = (response) => response.body;
 
 let token = null;
